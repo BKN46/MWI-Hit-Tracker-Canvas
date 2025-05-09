48fb2270f236a612305f0b3ec49088d221e8bd74
BKN46 <34993220+BKN46@users.noreply.github.com>
2025-05-10 00:28:18 +0800

    Minor fix

--- src/draw.js
+++ src/draw.js
@@ -217,7 +217,7 @@ class Projectile {
         let timeInAir = 80 / this.initialSpeed;
 
         // FPS因子，确保在不同FPS下效果一致
-        const fpsFactor = Math.min(Math.max(fps / 120, 0.125), 8); 
+        const fpsFactor = Math.min(Math.max(160 / fps, 0.125), 8); 
         this.gravity *= fpsFactor;
         timeInAir /= fpsFactor;
 
